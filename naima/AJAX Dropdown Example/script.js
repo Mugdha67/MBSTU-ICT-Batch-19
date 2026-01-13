e13d7787a81b8bb8451711dbb9eb47623d71c3a9
Kuldip Saha Mugdha <153665130+Mugdha67@users.noreply.github.com>
2026-01-13 11:54:34 +0600

    Update script.js

--- naima/AJAX Dropdown Example/script.js
+++ naima/AJAX Dropdown Example/script.js
@@ -1,22 +1,31 @@
-function loadStudents(dept) { 
-if (dept === "") { 
-document.getElementById("output").innerHTML = ""; 
-return; 
-} 
-var xhr = new XMLHttpRequest(); 
-xhr.open("GET", "info.json", true); 
-xhr.onreadystatechange = function () { 
-if (xhr.readyState === 4 && xhr.status === 200) { 
-var data = JSON.parse(xhr.responseText); 
-var html = "<ul>"; 
-for (var i = 0; i < data.length; i++) { 
-if (data[i].faculty === dept) { 
-html += "<li>" + data[i].dept + "</li>"; 
-} 
-} 
-html += "</ul>"; 
-document.getElementById("output").innerHTML = html; 
-} 
-}; 
-xhr.send(); 
+function loadDept(faculty) {
+
+    if (faculty === "") {
+        document.getElementById("output").innerHTML = "";
+        return;
+    }
+
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "info.json", true);
+
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+
+            var data = JSON.parse(xhr.responseText);
+            var html = "<ul>";
+
+            for (var i = 0; i < data.length; i++) {
+                if (data[i].faculty === faculty) {
+                    for (var j = 0; j < data[i].dept.length; j++) {
+                        html += "<li>" + data[i].dept[j] + "</li>";
+                    }
+                }
+            }
+
+            html += "</ul>";
+            document.getElementById("output").innerHTML = html;
+        }
+    };
+
+    xhr.send();
 }
