aeb545ad19cfe6195b819c317397e3b13b1a400d
Kuldip Saha Mugdha <153665130+Mugdha67@users.noreply.github.com>
2026-06-02 00:10:29 +0600

    Add files via upload

---
+++ 2-2/Project/SuperShop/src/main/java/com/example/supershop/web/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.supershop.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+	@GetMapping("/")
+	public String index(Model model) {
+		model.addAttribute("appName", "SuperShop");
+		return "index";
+	}
+}
+
+
